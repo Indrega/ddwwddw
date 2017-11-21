1327970b67802a7fcc9d02759a3ff889ae1aabda
Indrega <indroforconi@gmail.com>
2017-11-21 22:34:03 +0100

    ss

---
+++ miao.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+#include <process.h>
+#include <iostream>
+using namespace std;
+int main(int argc, char **argv)
+{
+    
+	for(int i=0;i<10;i++){
+        cout << i;
+        cout << "jeff";
+    }
+    system("pause");
+    return 0;
+}
